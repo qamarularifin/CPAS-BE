910ddea2319c6aeb39c6ee6fc3a488b0fa0ec6de
QIN YEE LIM <limqinyeelisa@gmail.com>
2023-10-07 20:25:10 +0800

    Added user

---
+++ UserWebAPI/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserWebApi.Models
+{
+    [Table("User", Schema = "dbo")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long UserId { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(320)]
+        public string Email { get; set; }
+
+        [MaxLength(100)]
+        public string Password { get; set; }
+
+        public int Role { get; set; }
+
+        [MaxLength(100)]
+        public string CreatedBy { get; set; }
+
+        public DateTime DateTimeCreated { get; set; }
+
+        [MaxLength(100)]
+        public string UpdatedBy { get; set; }
+
+        public DateTime DateTimeUpdated { get; set; }
+    }
+}
